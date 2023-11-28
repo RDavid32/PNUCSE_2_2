b26d916d34678473211e139a60ab72801e934257
RDavid32 <114576793+RDavid32@users.noreply.github.com>
2023-11-28 14:11:08 +0900

    xfrac change
    xfrac change

--- 16.xfrac/xfrac.py
+++ 16.xfrac/xfrac.py
@@ -1,74 +1,29 @@
-class xfrac:
-    def __init__(self,a,b,c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def evaluate(self): 
-        numerator, denominator = self.calculate_fraction()
-        common_divisor = self.gcd(numerator, denominator)
-        numerator //= common_divisor
-        denominator //= common_divisor
-        return numerator, denominator
-
-    def calculate_fraction(self):
-        # 연분수 계산
-        #분자, 분모
-        numerator_a, denominator_a = self.get_fraction_parts(self.a)
-        numerator_b, denominator_b = self.get_fraction_parts(self.b)
-        numerator_c, denominator_c = self.get_fraction_parts(self.c)
-
-        # 분모를 계산
-        denominator = denominator_a * denominator_b * numerator_c
-
-        # 분자를 계산
-        numerator = numerator_a * denominator_b * numerator_c + denominator_a * numerator_b * denominator_c
-        return numerator, denominator
-
-    def get_fraction_parts(self, fraction_part):
-        # 숫자 또는 xfrac 객체의 분자와 분모를 반환
-        if isinstance(fraction_part, xfrac):
-            return fraction_part.calculate_fraction()
-        else:
-            return fraction_part, 1
-
-    def gcd(self, a, b):
-        # 최대공약수 계산
-        while b != 0:
-            a, b = b, a % b
-        return a
-
-
-def parse(s):
-    stack = []
-    try:
-        for token in s:
-            if token == '(':
-                stack.append([])
-            elif token.isdigit():
-                stack[-1].append(int(token))
-            else:  
-                if stack:
-                    top = stack.pop()  
-
-                    if stack == []:
-                        stack.append(xfrac(*top))
-                    else:
-                        stack[-1].append(xfrac(*top))
-
-                else:
-                    exit(0)
-
-        while True:
-            if isinstance(stack[0], xfrac):
-                return stack[0]
-            stack = stack[0]
-    except:
-        print(-1)
-        exit(0)
+#icpc나간 동기의 조언을 받아 작성하였음
+import math
 
 n = int(input())
 s = input().strip().split()
+stack = []
 
-result = parse(s)
-print(*result.evaluate())
+for q in s:
+    try:
+        q = int(q)
+        stack.append([q,1])
+    except:
+        stack.append(q)
+    if len(stack) >4:
+        if stack[-5] == '(' and type(stack[-4]) == type(stack[-3]) == type(stack[-2]) == list and stack[-1] == ')':
+            numerator_a, denominator_a = stack[-4]
+            numerator_b, denominator_b = stack[-3]
+            numerator_c, denominator_c = stack[-2]
+            numerator = numerator_a * denominator_b * numerator_c + denominator_a * numerator_b * denominator_c
+            denominator = denominator_a * denominator_b * numerator_c
+            for _ in range(5):stack.pop()
+            num = math.gcd(numerator,denominator)
+            stack.append([numerator//num, denominator//num])
+
+            
+if len(stack) == 1 and type(stack[0]) == list:
+    print(*stack[0])
+else:
+    print(-1)
